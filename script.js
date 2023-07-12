3f3294b87c0f10913bb6a51d112d1ef4d8beb9b9
Natanael Borge <nborge@mejialora.com>
2023-07-12 11:02:19 -0400

    Barra de navegacion correccion responsive y animacion de scroll

--- script.js
+++ script.js
@@ -33,3 +33,32 @@ function updateSlider() {
 
 
 startAutoSlide(); // Iniciar el slider automático al cargar la página
+
+//FadeIN
+function debounce(func, wait = 20, immediate = true) {
+  let timeout;
+  return function() {
+    const context = this, args = arguments;
+    const later = function() {
+      timeout = null;
+      if (!immediate) func.apply(context, args);
+    };
+    const callNow = immediate && !timeout;
+    clearTimeout(timeout);
+    timeout = setTimeout(later, wait);
+    if (callNow) func.apply(context, args);
+  };
+}
+
+function checkFadeIn() {
+  const fadeInElements = document.querySelectorAll('.fade-in');
+  fadeInElements.forEach(function(element) {
+    const fadeInPosition = element.getBoundingClientRect().top + 20;
+    if (fadeInPosition < window.innerHeight) {
+      element.classList.add('fade-in-show');
+    }
+  });
+}
+
+window.addEventListener('DOMContentLoaded', checkFadeIn);
+window.addEventListener('scroll', debounce(checkFadeIn));
