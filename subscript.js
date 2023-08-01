0e3c5789c22e3d984e45b91de606014c5b7a80aa
Natanael Borge <nborge@mejialora.com>
2023-08-01 11:40:22 -0400

    Barra de navegación FIXED

--- subscript.js
+++ subscript.js
@@ -26,3 +26,27 @@ function checkFadeIn() {
 
 window.addEventListener('DOMContentLoaded', checkFadeIn);
 window.addEventListener('scroll', debounce(checkFadeIn));
+
+//Barra de navegación FIXED
+window.addEventListener("load", function() {
+  // Obtén la barra de navegación
+  var barra = document.getElementById("mi-barra");
+  
+  // Obtén la posición inicial de la barra de navegación
+  var barraOffset = barra.offsetTop;
+
+  // Agrega un evento para detectar el scroll de la página
+  window.addEventListener("scroll", function() {
+    // Obtén la posición actual del scroll
+    var scrollActual = window.scrollY;
+
+    // Comprueba si la posición del scroll supera la posición inicial de la barra
+    if (scrollActual > barraOffset) {
+      // Si es así, agrega la clase CSS 'fixed' para hacer la barra fija
+      barra.classList.add("fixed");
+    } else {
+      // Si el scroll retrocede, elimina la clase 'fixed' para que la barra vuelva a su posición original
+      barra.classList.remove("fixed");
+    }
+  });
+});
